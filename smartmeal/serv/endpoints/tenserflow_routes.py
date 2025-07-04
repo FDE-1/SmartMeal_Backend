ebf4cdda969a7acba639be2fb7109737fe6f61b6
Philemon3111 <99840844+Philemon3111@users.noreply.github.com>
2025-07-04 20:42:09 +0200

    fix: change url

--- smartmeal/serv/endpoints/tenserflow_routes.py
+++ smartmeal/serv/endpoints/tenserflow_routes.py
@@ -13,7 +13,7 @@ import threading
 from functools import wraps
 api = Namespace('tenserflow', description='Test TenserFlow')
 
-API_BASE_URL = 'http://beaa-2a01-e0a-ee7-db30-4042-e6e2-70b2-fdb1.ngrok-free.app'
+API_BASE_URL = 'http://c2f8-2a01-e0a-ee7-db30-2467-8e1b-c2df-bbc7.ngrok-free.app'
 
 ingredient_model = api.model('Ingredient', {
     'name': fields.String(required=True, description='Nom de l\'ingrédient'),
