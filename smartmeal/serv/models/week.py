353f1bc81d46f108c5de70442a36322456a0b131
Philemon3111 <99840844+Philemon3111@users.noreply.github.com>
2025-09-22 18:06:28 +0200

    feat: modify the unique restraint

--- smartmeal/serv/models/week.py
+++ smartmeal/serv/models/week.py
@@ -15,7 +15,3 @@ class Week(db.Model):
     vendredi = db.Column(ARRAY(JSONB), default=[])
     samedi = db.Column(ARRAY(JSONB), default=[])
     dimanche = db.Column(ARRAY(JSONB), default=[])
-
-    __table_args__ = (
-        db.UniqueConstraint('user_id', 'week_number', name='uq_user_week'),
-    )
